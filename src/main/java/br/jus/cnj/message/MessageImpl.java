235e8d82eb15133847a9787cd425e9a6ff3a15e4
welder496 <welder496@gmail.com>
2016-04-15 11:43:01 -0300

    Autoria!!

--- src/main/java/br/jus/cnj/message/MessageImpl.java
+++ src/main/java/br/jus/cnj/message/MessageImpl.java
@@ -2,6 +2,12 @@ package br.jus.cnj.message;
 
 import javax.jws.WebService;
 
+/**
+ * Exemplo de WebService para Jboss
+ * @author welder
+ *
+ */
+
 @WebService(serviceName = "MessageInt", portName = "MessageInt", name = "MessageInt", endpointInterface = "br.jus.cnj.message.MessageInt")
 public class MessageImpl implements MessageInt {
 
